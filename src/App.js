eb46867ab4d43be709ac17a8772233ecb47dd121
suvinjosh <crsj777@gmail.com>
2023-12-11 13:13:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
+import "./App.css";
 function App() {
+  const [list, setList] = useState([]);
+  const [value, setValue] = useState("");
+  const addToList = () => {
+    let tempArr = list;
+    tempArr.push(value);
+    setList(tempArr);
+    setValue("");
+  };
+ const deleteItem = (index) => {
+ let temp = list.filter((item, i) => i !== index);
+    setList(temp);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <fieldset>
+        <h>Add Product to List</h><br></br>
+    <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
+    <button onClick={addToList}> Click to Add </button><br></br><br></br>
+    <h>Product Catalog</h><br></br>
+    <ol>
+        {list.map((item, i) => <li onClick={() => deleteItem(i)}>{item} </li>)}
+    </ol>
+    <h>Click on Product to Delete</h><br></br>
+    </fieldset></div>
   );
-}
+}export default App;
 
-export default App;
